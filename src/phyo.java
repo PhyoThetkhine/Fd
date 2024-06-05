d0b35bac9f4a11635b862a18ca1bbba7bcf8b7bc
Phyo <phyo@example.com>
2024-06-05 22:36:51 +0630

    first commit

---
+++ src/phyo.java
@@ -0,0 +1,18 @@
+public class phyo {
+    public static void main (String [] args){
+
+        int a = 10 + 20;
+        int b = 30 / 5;
+        int c = a * b;
+        System.out.println(c);
+
+        int A = 65;
+        System.out.println(A);
+        boolean JavaFun = true;
+        boolean FishTasty = false;
+        System.out.println("Is Java fun? " + JavaFun);
+        System.out.println("Is fish tasty? " + FishTasty);
+
+
+    }
+}
